f11187e3ecd565cf088edb7f1a15a0f4db04502c
Taiwo Ogunola <uniqueomokenny@gmail.com>
2020-01-26 23:29:44 +0100

    nav header done

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import { Route, Switch } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/home/home';
 import ShopPage from './pages/shop/shop';
+import Header from './components/header/header';
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route exact path='/shop' component={ShopPage} />
