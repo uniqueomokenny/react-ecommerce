815ecb9e6715fb224218f2e17045398eced9cb1b
Taiwo Ogunola <uniqueomokenny@gmail.com>
2020-01-26 19:32:45 +0100

    split homepage to components and finished styling

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,45 +1,12 @@
 import React from 'react';
 import './home.styles.scss';
+import MenuItem from '../../components/menu-item/menu-item';
+import Directory from '../../components/directory/directory';
 
 export default function HomePage() {
   return (
     <div className='homepage'>
-      <div className='directory-menu'>
-        <div className='menu-item'>
-          <div className='content'>
-            <h1 className='title'>HATS</h1>
-            <span className='subtitle'>SHOP NOW</span>
-          </div>
-        </div>
-
-        <div className='menu-item'>
-          <div className='content'>
-            <h1 className='title'>JACKETS</h1>
-            <span className='subtitle'>SHOP NOW</span>
-          </div>
-        </div>
-
-        <div className='menu-item'>
-          <div className='content'>
-            <h1 className='title'>SNEAKERS</h1>
-            <span className='subtitle'>SHOP NOW</span>
-          </div>
-        </div>
-
-        <div className='menu-item'>
-          <div className='content'>
-            <h1 className='title'>WOMENS</h1>
-            <span className='subtitle'>SHOP NOW</span>
-          </div>
-        </div>
-
-        <div className='menu-item'>
-          <div className='content'>
-            <h1 className='title'>MENS</h1>
-            <span className='subtitle'>SHOP NOW</span>
-          </div>
-        </div>
-      </div>
+      <Directory />
     </div>
   );
 }
