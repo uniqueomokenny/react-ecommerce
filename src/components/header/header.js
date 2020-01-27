f0316218502f22c8a18ec645d7a2077724ac2c0d
Taiwo Ogunola <uniqueomokenny@gmail.com>
2020-01-27 23:47:20 +0100

    add cart dropdown component

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -6,8 +6,10 @@ import { ReactComponent as Logo } from '../../assets/header.svg';
 
 import './header.styles.scss';
 import { auth } from '../../firebase/firebase.util';
+import CartIcon from '../cart-icon/cart-icon';
+import CartDropdown from '../cart-dropdown/cart-dropdown';
 
-function Header({ currentUser }) {
+function Header({ currentUser, hidden }) {
   return (
     <div className='header'>
       <Link className='logo-container' to='/'>
@@ -31,13 +33,16 @@ function Header({ currentUser }) {
             SIGN IN
           </Link>
         )}
+        <CartIcon />
       </div>
+      {hidden ? null : <CartDropdown />}
     </div>
   );
 }
 
-const mapStateToProps = state => ({
-  currentUser: state.user.currentUser
+const mapStateToProps = ({ user: { currentUser }, cart: { hidden } }) => ({
+  currentUser,
+  hidden
 });
 
 export default connect(mapStateToProps)(Header);
