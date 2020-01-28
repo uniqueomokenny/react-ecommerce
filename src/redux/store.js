fd7042dd175c24bc11bf4f30c0e4db336c96400c
Taiwo Ogunola <uniqueomokenny@gmail.com>
2020-01-28 15:10:01 +0100

    added node development flag redux middlewares

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,22 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import { persistStore } from 'redux-persist';
 import logger from 'redux-logger';
+import thunk from 'redux-thunk';
 import rootReducer from './root-reducer';
 
-const middlewares = [logger];
+const middlewares = [];
 
-const store = createStore(rootReducer, applyMiddleware(...middlewares));
+if (process.env.NODE_ENV === 'development') {
+  middlewares.push(logger);
+  middlewares.push(thunk);
+}
+
+const composeEnhances = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(
+  rootReducer,
+  composeEnhances(applyMiddleware(...middlewares))
+);
 
 export const persistor = persistStore(store);
 
