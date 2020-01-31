08cb81fc319aab066e0adeca5ae1c85cb5c5cb0c
Taiwo Ogunola <uniqueomokenny@gmail.com>
2020-01-31 18:42:34 +0100

    express server for stripe payment

--- client/src/components/stripe-button/stripe-button.js
+++ client/src/components/stripe-button/stripe-button.js
@@ -1,13 +1,29 @@
 import React from 'react';
 import StripeCheckout from 'react-stripe-checkout';
+import axios from 'axios';
 
 export default function StripeCheckoutButton({ price }) {
   const priceForStripe = price * 100;
   const publishableKey = 'pk_test_jPO1MtcAbyylYSkZe0RGmRHw00QcwqEVKQ';
 
   const onToken = token => {
-    console.log(token);
-    alert('Payment Successful');
+    axios({
+      url: 'payment',
+      method: 'post',
+      data: {
+        amount: priceForStripe,
+        token
+      }
+    })
+      .then(res => {
+        alert('Payment successful');
+      })
+      .catch(err => {
+        console.log('Payment error', JSON.parse(err));
+        alert(
+          'There was an error with your payment. You can make use of the test credit card'
+        );
+      });
   };
 
   return (
